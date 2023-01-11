5ff0a3b80cfea96efe4c74e9860af780732fba5a
Anatolii Tarasov <tarasov.front.dev@gmail.com>
2023-01-11 19:28:34 +0000

    Keep learning JS. Level 2. Objects. Making objects via variables

--- learn.js.ru/js/script_L2.js
+++ learn.js.ru/js/script_L2.js
@@ -2,26 +2,37 @@
 
 // /************Objects************/
 
-const user = {
-	name: "Tolya",
-	age: 33,
-};
-console.log(user);
+// const user = {
+// 	name: "Tolya",
+// 	age: 33,
+// };
+// console.log(user);
 
-user.admin = true;
-console.log(user);
+// user.admin = true;
+// console.log(user);
 
-user["sex-identify"] = "man";
-console.log(user);
+// user["sex-identify"] = "man";
+// console.log(user);
 
-delete user.sex;
-console.log(user);
+// delete user.sex;
+// console.log(user);
 
 
-let fruit = prompt("Какой фрукт купить?", "apple");
+// let fruit = prompt("Какой фрукт купить?", "apple");
 
-let bag = {
-  [fruit]: 5, // имя свойства будет взято из переменной fruit
-};
+// let bag = {
+//   [fruit]: 5, // имя свойства будет взято из переменной fruit
+// };
 
-console.log(`You should buy: "${fruit} - ${bag.apple} qty"`);
+// console.log(`You should buy: "${fruit} - ${bag.apple} qty"`);
+
+
+function makeUser(name, age) {
+  return {
+    name, // same as name: name,
+    age   // same as age: age,
+  };
+}
+
+const user = makeUser("Tolya", 33);
+console.log(user.name); // Tolya
