c9d9c462fb939ee1bf3b3c66fedf8d28dc4cd7b6
Anatolii Tarasov <tarasov.front.dev@gmail.com>
2023-01-11 19:36:55 +0000

    Keep learning JS. Level 2. Objects. In to check values in object

--- learn.js.ru/js/script_L2.js
+++ learn.js.ru/js/script_L2.js
@@ -27,12 +27,27 @@
 // console.log(`You should buy: "${fruit} - ${bag.apple} qty"`);
 
 
-function makeUser(name, age) {
-  return {
-    name, // same as name: name,
-    age   // same as age: age,
-  };
-}
+// /************Shorthand************/
+// function makeUser(name, age) {
+//   return {
+//     name, // same as name: name,
+//     age   // same as age: age,
+//   };
+// }
 
-const user = makeUser("Tolya", 33);
-console.log(user.name); // Tolya
+// const user = makeUser("Tolya", 33);
+// console.log(user.name); // Tolya
+
+
+// /************"In" to check values in object************/
+const user = {
+	name: "Tolya",
+	age: 33,
+};
+
+const rights = "admin";
+user[rights] = true;
+
+console.log("name" in user);
+console.log("admin" in user);
+console.log(user.name + " is " + rights);
